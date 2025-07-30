ea6a1c2a8a285ac0fe3ca1a5c8294d8db508141c
coryo12345 <corydio12@gmail.com>
2025-07-29 20:48:03 -0400

    test item works, and fix missing elytra equipment

--- scripts/migrate.js
+++ scripts/migrate.js
@@ -23,9 +23,9 @@ for (const tier of tiers) {
             style = file.replace("_layer_1.png", "")
             makeEquipment = true
             const src = path.join(optifineModels, tier, file)
-            const destinationDir = path.join(rootDir, "textures", "entity", "equipment", "humanoid", tier) 
+            const destinationDir = path.join(rootDir, "textures", "entity", "equipment", "humanoid", tier)
             const destination = path.join(destinationDir, style + ".png")
-            fs.mkdirSync(destinationDir, {recursive: true})
+            fs.mkdirSync(destinationDir, { recursive: true })
             fs.copyFileSync(src, destination)
         }
 
@@ -33,30 +33,45 @@ for (const tier of tiers) {
             style = file.replace("_layer_2.png", "")
             makeEquipment = true
             const src = path.join(optifineModels, tier, file)
-            const destinationDir = path.join(rootDir, "textures", "entity", "equipment", "humanoid_leggings", tier) 
+            const destinationDir = path.join(rootDir, "textures", "entity", "equipment", "humanoid_leggings", tier)
             const destination = path.join(destinationDir, style + ".png")
-            fs.mkdirSync(destinationDir, {recursive: true})
+            fs.mkdirSync(destinationDir, { recursive: true })
+            fs.copyFileSync(src, destination)
+        }
+
+        if (file.endsWith("_elytra.png")) {
+            style = file.replace("_elytra.png", "")
+            makeEquipment = true
+            const src = path.join(optifineModels, tier, file)
+            const destinationDir = path.join(rootDir, "textures", "entity", "equipment", "wings", tier)
+            const destination = path.join(destinationDir, style + ".png")
+            fs.mkdirSync(destinationDir, { recursive: true })
             fs.copyFileSync(src, destination)
         }
 
         if (makeEquipment) {
-            const obj = {
-                layers: {
-                    humanoid: [
-                        {
-                            texture: tier + "/" + style
-                        }
-                    ],
-                    humanoid_leggings: [
-                        {
-                            texture: tier + "/" + style
-                        }
-                    ]
-                }
+            const obj = { layers: {} }
+            if (tier == 'elytra') {
+                obj.layers['wings'] = [
+                    {
+                        texture: tier + '/' + style
+                    }
+                ]
+            } else {
+                obj.layers['humanoid'] = [
+                    {
+                        texture: tier + "/" + style
+                    }
+                ]
+                obj.layers['humanoid_leggings'] = [
+                    {
+                        texture: tier + "/" + style
+                    }
+                ]
             }
             const outDir = path.join(rootDir, "equipment", tier)
             const outfile = path.join(outDir, style + ".json")
-            fs.mkdirSync(outDir, {recursive: true})
+            fs.mkdirSync(outDir, { recursive: true })
             fs.writeFileSync(outfile, JSON.stringify(obj, null, 4))
         }
     }
